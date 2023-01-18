5777499dd9c0e37854de569af282b239c6b94e5c
RyannKim327 <rksesgundo123@gmail.com>
2023-01-19 05:44:33 +0800

    01-19-23 05:44

--- index.js
+++ index.js
@@ -14,13 +14,37 @@ app.get("/", (req, res) => {
 })
 
 app.post("/login", body, (req, res) => {
-	console.log("Called " + JSON.stringify(req.body))
+	let db = JSON.parse(fs.readFileSync("data.json", "utf-8"))
 	let user = req.body.username
 	let pass = req.body.password
-	console.log(user + " " + pass)
 	let json = {
-		id: 0,
-		user
+		exists: false
+	}
+	if(db.users[user.toLowerCase()] == undefined){
+		let id = Object.keys(db.users).length || 1
+		db.users[user.toLowerCase()] = {
+			id,
+			username: user,
+			password: pass
+		}
+		fs.writeFileSync('data.json', JSON.stringify(db), "utf-8")
+		json = {
+			exists: true,
+			id,
+			user
+		}
+	}else{
+		let db2 = JSON.parse(fs.readFileSync("data.json", "utf-8"))
+		let usr = user.toLowerCase()
+		if(db2.users[usr]['password'] == pass){
+			let id = db2.users[usr]['id']
+			let user = db2.users[usr]['username']
+			json = {
+				exists: true,
+				id,
+				user
+			}
+		}
 	}
 	res.send(JSON.stringify(json))
 })
