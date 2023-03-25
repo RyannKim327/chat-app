d9f78ac405b9d224371ec631ee51d99e3b1d7e01
RyannKim327 <rksesgundo123@gmail.com>
2023-03-26 06:15:20 +0800

    03-26-23 06:15

--- src/script.js
+++ src/script.js
@@ -1,28 +1,6 @@
 let id = (_name_) => {
 	return document.getElementById(_name_)
 }
-let setCookie = (__name__, __data__) => {
-	const date = new Date()
-	date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000))
-	let xp = `expires=${date.toUTCString()}`
-	document.cookie = `${__name__}=${__data__};${xp};path=/`
-}
-let	getCookie = (__name__) => {
-	let cook = document.cookie
-	let name = `${__name__}=`	
-	let decode = decodeURIComponent(cook)
-	let spl = decode.split(";")
-	for(let i in spl){
-		let kie = spl[i]
-		while(kie[0] == " "){
-			kie = kie.substring(1)
-		}
-		if(kie.indexOf(name) == 0){
-			return kie.substring(name.length, kie.length)
-		}
-	}
-	return ""
-}
 
 let credentials = {
 	id: -1,
