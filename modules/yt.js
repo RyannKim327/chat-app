a3c0458f91f93a07cd3c16578a64c9b4bca51758
RyannKim327 <rksesgundo123@gmail.com>
2023-11-04 08:01:19 +0800

    11/4/2023 8:01:18 AM [Autogit]

--- modules/yt.js
+++ modules/yt.js
@@ -1,1 +1,8 @@
+const axios = require("axios")
 const fs = require("fs")
+const YoutubeMusicApi = require('youtube-music-api')
+const yt = new YoutubeMusicApi()
+const ytdl = require('ytdl-core');
+const ffmpeg = require('@ffmpeg-installer/ffmpeg')
+const ffmpegs = require('fluent-ffmpeg')
+ffmpegs.setFfmpegPath(ffmpeg.path)
