508cab0c7de7ece1f20ec21e5c6ba8a8607f014e
RyannKim327 <rksesgundo123@gmail.com>
2023-11-04 08:13:49 +0800

    11/4/2023 8:13:49 AM [Autogit]

--- modules/yt.js
+++ modules/yt.js
@@ -19,11 +19,12 @@ module.exports = async (msg_id, user, date, title, oldM) => {
 				"id": msg_id,
 				"user": "Music",
 				"rank": "bot",
-				"txt": "Music not found",
+				"txt": "Music not found, please check your query add some singer on your title",
 				"time": date.getTime(),
 				"reply": -1
 			})
 		}
+		
 	}catch(e){
 		return json.chats.append({
 			"id": msg_id,
