e32f579d9cc834512687c014daeb540b3704f557
RyannKim327 <rksesgundo123@gmail.com>
2023-11-04 08:24:33 +0800

    11/4/2023 8:24:33 AM [Autogit]

--- modules/yt.js
+++ modules/yt.js
@@ -15,24 +15,24 @@ module.exports = async (msg_id, user, date, title, oldM) => {
 		await yt.initalize()
 		let search = await yt.search(title.replace(/[^\w\s]/gi, ''))
 		if(search.content.length <= 0){
-			return json.chats[msg_id]({
+			return json.chats[msg_id] = {
 				"id": msg_id,
 				"user": "Music",
 				"rank": "bot",
 				"txt": "Music not found, please check your query add some singer on your title",
 				"time": date.getTime(),
 				"reply": -1
-			})
+			}
 		}
 		if(search.content[0].videoID == undefined){
-			return json.chats[msg_id]({
+			return json.chats[msg_id] = {
 				"id": msg_id,
 				"user": "Music",
 				"rank": "bot",
 				"txt": "Music not found, please check your query add some singer on your title",
 				"time": date.getTime(),
 				"reply": -1
-			})
+			}
 		}
 		const url  = `https://www.youtube.com/watch?v=${music.content[0].videoId}`
 		const strm =  ytdl(url, {
